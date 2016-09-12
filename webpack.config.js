eef4187d6dc6284fcb8a9a015a08a719659d975a
apigareva <pigareva.alexandra@yandex.ru>
2016-09-12 15:32:00 +0300

    update dist

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
     output: {
         path: path.join(__dirname, 'dist'),
         publicPath: '/',
-        filename: '[name].js'
+        filename: 'build.js'
     },
 
     watch: true,
